54fb30dfe5afeb9f435be7761d64e96ac779626b
Heli Vepsäläinen <L4136@student.jamk.fi>
2018-02-21 21:16:00 +0200

    T3t6

---
+++ Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3t6
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public String Breed { get; set; }
+        public String RegNumber { get; set; }
+
+        public override string ToString()
+        {
+            return "Dog: " + Name + " is a " + Breed + " and is registered under the registration number " + RegNumber;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace T3t6
     {
         static void Main(string[] args)
         {
+            Dog[] dogs = new Dog[3];
+            dogs[0] = new Dog { Name = "Bobe", Breed = "mudi", RegNumber = "FI12345/14" };
+            dogs[1] = new Dog { Name = "Alfa", Breed = "german shepherd", RegNumber = "FI98765/17" };
+            dogs[2] = new Dog { Name = "Ritu", Breed = "malinois", RegNumber = "FI91827/05" };
+            foreach (Dog dog in dogs)
+            {
+                Console.WriteLine(dog.ToString());
+
+            }
         }
     }
 }
